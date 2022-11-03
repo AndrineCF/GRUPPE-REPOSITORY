8cb05101759902d9c592cdbaa13441d6420033eb
andrine <andrine.celine.flatby@gmail.com>
2022-11-03 14:11:00 +0100

    La til const variabler

--- how_we_roll/output.js
+++ how_we_roll/output.js
@@ -1,15 +1,18 @@
 var c=document.getElementById("Linje1")
 var ctx=c.getContext("2d");
 
+const y = 15
+const radius = 12.5
+
 //Laget linje 1
 ctx.beginPath();
-ctx.moveTo(0,15)
+ctx.moveTo(0,y)
 ctx.lineTo(500,15)
 ctx.stroke();
 
 //Lage kule
 ctx.beginPath();
-ctx.arc(50, 15, 12.5, 0, 2 * Math.PI);
+ctx.arc(50, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.fillStyle = "#00FFF0";
 ctx.fill();
