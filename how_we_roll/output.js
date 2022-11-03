922f23d951767c0abec44ff05c56bb14f853aaf8
Felix12390 <felixaa@uia.no>
2022-11-03 14:07:58 +0100

    laget første ball

--- how_we_roll/output.js
+++ how_we_roll/output.js
@@ -1,6 +1,15 @@
 var c=document.getElementById("Linje1")
 var ctx=c.getContext("2d");
+
+//Laget linje 1
 ctx.beginPath();
-ctx.moveTo(0,12.5)
-ctx.lineTo(500,12.5)
+ctx.moveTo(0,15)
+ctx.lineTo(500,15)
 ctx.stroke();
+
+//Lage kule
+ctx.beginPath();
+ctx.arc(50, 15, 12.5, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "#00FFF0";
+ctx.fill();
