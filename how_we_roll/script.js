f0dd169ac6657534ff9253d240925e6f9bdc62a1
RuneU <runeundheim@live.no>
2022-11-07 13:57:00 +0100

    Made variables

--- how_we_roll/script.js
+++ how_we_roll/script.js
@@ -4,4 +4,11 @@ function myBtn() {
    var morningTime = document.getElementById("wakeupTime").value
     console.log(morningTime)
 
+    var workTogeth = document.getElementById("workTogether").value
+    console.log(workTogeth)
+    var story = document.getElementById("backStory").value
+    console.log(story)
+    var talkItOut = document.getElementById("talkItOut").value
+    console.log(talkItOut)
+
 }
