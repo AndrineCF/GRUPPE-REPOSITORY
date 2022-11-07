04f84d985e630fab2f0e68e6c1c71c4dd8bd3a70
RuneU <runeundheim@live.no>
2022-11-07 14:42:11 +0100

    Made object formData

--- how_we_roll/script.js
+++ how_we_roll/script.js
@@ -1,24 +1,22 @@
 function myBtn() {
-    console.log("kan jeg dette?")
 
    var morningTime = document.getElementById("wakeupTime").value
-    console.log(morningTime)
-
     var workTogeth = document.getElementById("workTogether").value
-    console.log(workTogeth)
     var story = document.getElementById("backStory").value
-    console.log(story)
     var talkItOut = document.getElementById("talkItOut").value
-    console.log(talkItOut)
     var processor = document.getElementById("Processor").value
-    console.log(processor)
     var chat = document.getElementById("Schedule").value
-    console.log(chat)
     var interaction = document.getElementById("chatting").value
-    console.log(interaction)
     var workTime = document.getElementById("workTime").value
-    console.log(workTime)
     var plan = document.getElementById("planning").value
-    console.log(plan)
+    
 
+    
+    var formData = {};
+    formData['planning'] = plan;
+    formData['workTime'] = workTime
+    formData['chatting'] = interaction
+    formData['Schedule'] = chat
+    formData['Processor'] = processor
+    console.log(formData.planning)
 }
