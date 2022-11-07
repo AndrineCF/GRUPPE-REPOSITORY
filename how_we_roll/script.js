8d5306f89ae1ee94cd9f6a76c8ff34a9ca207e0e
jonhovd <jonhovd@outlook.com>
2022-11-07 13:48:47 +0100

    jobbet mer på script

--- how_we_roll/script.js
+++ how_we_roll/script.js
@@ -1,1 +1,7 @@
-document.getElementById()
+function myBtn() {
+    console.log("kan jeg dette?")
+
+   var morningTime = document.getElementById("wakeupTime").value
+    console.log(morningTime)
+
+}
