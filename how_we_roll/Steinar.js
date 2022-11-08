2aa889ba50c737a46dd111c018278f778a134d70
Steinar Blom <112163114+steina021@users.noreply.github.com>
2022-11-08 10:59:04 +0100

    laget flere sirkler på linje 4 og 5

--- how_we_roll/Steinar.js
+++ how_we_roll/Steinar.js
@@ -1,40 +1,70 @@
-var c=document.getElementById("Linje4")
-var ctx=c.getContext("2d");
+var c = document.getElementById("Linje4")
+var ctx = c.getContext("2d");
 
 //Laget linje 4
 ctx.beginPath();
-ctx.moveTo(0,y)
-ctx.lineTo(500,15)
+ctx.moveTo(0, y)
+ctx.lineTo(500, 15)
 ctx.stroke();
 
 //Lage kule 1
 ctx.beginPath();
-ctx.arc(100, y, radius, 0, 2 * Math.PI);
+ctx.arc(50, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
 ctx.fillStyle = "#00FFF0";
 ctx.fill();
 
 //Lage kule 2
 ctx.beginPath();
-ctx.arc(450,y,radius,0,2* Math.PI);
+ctx.arc(475, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
-ctx.fillStyle="blue";
+ctx.fillStyle = "blue";
 ctx.fill();
 
 //Lage kule 3
 ctx.beginPath();
-ctx.arc(400,y,radius,0,2* Math.PI);
+ctx.arc(400, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
-ctx.fillStyle="red";
+ctx.fillStyle = "red";
 ctx.fill();
 
-var c=document.getElementById("Linje5")
-var ctx=c.getContext("2d");
+//Lage kule 4
+ctx.beginPath();
+ctx.arc(300, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "green";
+ctx.fill();
+
+//Lage kule 5
+ctx.beginPath();
+ctx.arc(125, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "pink";
+ctx.fill();
+
+//Lage kule 6
+ctx.beginPath();
+ctx.arc(225, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "purple";
+ctx.fill();
+
+//Lage kule 7
+ctx.beginPath();
+ctx.arc(475, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "black";
+ctx.fill();
+
+
+
+var c = document.getElementById("Linje5")
+var ctx = c.getContext("2d");
 
 //Laget linje 5
 ctx.beginPath();
-ctx.moveTo(0,y)
-ctx.lineTo(500,15)
+ctx.moveTo(0, y)
+ctx.lineTo(500, 15)
 ctx.stroke();
 
 //Lage kule 1
@@ -46,15 +76,43 @@ ctx.fill();
 
 //Lage kule 2
 ctx.beginPath();
-ctx.arc(350,y,radius,0,2* Math.PI);
+ctx.arc(350, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
-ctx.fillStyle="blue";
+ctx.fillStyle = "blue";
 ctx.fill();
 
 //Lage kule 3
 ctx.beginPath();
-ctx.arc(75,y,radius,0,2* Math.PI);
+ctx.arc(50, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
-ctx.fillStyle="red";
+ctx.fillStyle = "red";
 ctx.fill();
 
+//Lage kule 4
+ctx.beginPath();
+ctx.arc(250, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "green";
+ctx.fill();
+
+
+//Lage kule 5
+ctx.beginPath();
+ctx.arc(425, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "pink";
+ctx.fill();
+
+//Lage kule 6
+ctx.beginPath();
+ctx.arc(150, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "purple";
+ctx.fill();
+
+//Lage kule 7
+ctx.beginPath();
+ctx.arc(475, y, radius, 0, 2 * Math.PI);
+ctx.stroke();
+ctx.fillStyle = "black";
+ctx.fill();
--- how_we_roll/output.js
+++ how_we_roll/output.js
@@ -1,37 +1,16 @@
+var c=document.getElementById("Linje1")
+var ctx=c.getContext("2d");
+
 const y = 15
 const radius = 12.5
-const width = 500
-
-//regner ut koordinater for sirker på y-aksen
-function utregningOutput(prosent) {
-    return (prosent * width) / 100
-}
-
-// function lageLine(id) {
-//     var c=document.getElementById(id)
-//     var ctx=c.getContext("2d");
-
-//     //lage linje
-//     ctx.beginPath();
-//     ctx.moveTo(0,y)
-//     ctx.lineTo(width,15)
-//     ctx.stroke();
-// }
-
-// lageLine("linje1")
-
-
-var id = "linje1"
-var c=document.getElementById(id)
-var ctx=c.getContext("2d");
 
 //Laget linje 1
 ctx.beginPath();
 ctx.moveTo(0,y)
-ctx.lineTo(width,15)
+ctx.lineTo(500,15)
 ctx.stroke();
 
-//Lage kule 1
+//Lage kule
 ctx.beginPath();
 ctx.arc(50, y, radius, 0, 2 * Math.PI);
 ctx.stroke();
@@ -44,3 +23,4 @@ ctx.arc(300,y,radius,0,2* Math.PI);
 ctx.stroke();
 ctx.fillStyle="blue";
 ctx.fill();
+
